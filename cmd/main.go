e9b5fc7c833e4c388ddc33483ed610e6ebb77371
jamm3e3333 <jamm3e3333@gmail.com>
2024-06-02 20:23:23 +0200

    fix(cfg): add cfg http values with time

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jamm3e3333/start/pkg/http"
@@ -12,13 +13,19 @@ func main() {
 	lg := logger.NewLogger()
 
 	gin.SetMode(gin.ReleaseMode)
-	ge := gin.Default()
+	ge := gin.New()
+
+	ge.Any("/", func(c *gin.Context) {
+		c.AbortWithStatusJSON(200, gin.H{
+			"message": "Hello World",
+		})
+	})
 
 	httpServer := http.NewServer(&http.Config{
 		Port:            8080,
-		ReadTimeout:     5,
-		WriteTimeout:    5,
-		ShutdownTimeout: 5,
+		ReadTimeout:     10 * time.Second,
+		WriteTimeout:    30 * time.Second,
+		ShutdownTimeout: 15 * time.Second,
 		Handler:         ge,
 	})
 	errChan := httpServer.Start()
